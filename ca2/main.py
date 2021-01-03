7c6d7ff1a88675ffe31a6ec32a498c91ca8966c2
Nupur Sonve <75483127+nupursonve@users.noreply.github.com>
2021-01-03 22:43:59 +0000

    Create main.py

---
+++ ca2/main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, jsonify, session, redirect
+app = Flask(__name__)
+
+# branch
+@app.route('/')
+def branch():
+    return render_template("branch.html")
+
+if __name__ == "__main__":
+  app.run()
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
+
+if __name__ == "__main__":
+  app.run()
