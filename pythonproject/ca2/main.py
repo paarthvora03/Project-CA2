9bcd378e4e60f530e40b43fa8d0ed3ccb91ef780
Nupur Sonve <75483127+nupursonve@users.noreply.github.com>
2021-01-04 11:48:08 +0000

    Update main.py

--- pythonproject/ca2/main.py
+++ pythonproject/ca2/main.py
@@ -8,3 +8,11 @@ def branch():
 
 if __name__ == "__main__":
   app.run()
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
+
+if __name__ == "__main__":
+  app.run()
